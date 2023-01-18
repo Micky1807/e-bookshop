db9b7b43bd0c0523c814a301a62024da83459966
Mayank Singodia <75880137+Micky1807@users.noreply.github.com>
2023-01-18 11:06:09 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 export const Connection = async (URL) => {
     try {
       
-        
+        const URL = process.env.MONGODB_URI || `mongodb://${USERNAME}:${PASSWORD}@ac-s1hjcab-shard-00-00.ftazuox.mongodb.net:27017,ac-s1hjcab-shard-00-01.ftazuox.mongodb.net:27017,ac-s1hjcab-shard-00-02.ftazuox.mongodb.net:27017/?ssl=true&replicaSet=atlas-du1g60-shard-0&authSource=admin&retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
 
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
         console.log('Database connected succesfully');
